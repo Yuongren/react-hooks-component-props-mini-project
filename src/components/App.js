c776cb98eef843dd900f8ef970f1cbbf8d150c02
gitYoungren <youngrengitonga@gmail.com>
2023-01-13 13:36:05 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,19 @@
 import React from "react";
 import blogData from "../data/blog";
-import About from "./About";
-import ArticleList from "./ArticleList";
-import Header from "./Header";
-import Article from "./Article";
+import Header from "./Header"
+import About from "./About"
+import ArticleList from "./ArticleList"
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      <About/>
-      <ArticleList/>
       <Header name={blogData.name}/>
-      <Article/>
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts}/>
     </div>
-  )
+  );
 }
 
 export default App;
